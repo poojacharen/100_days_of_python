90dd64be1429a24745afa5d5c14555ccae954b2b
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-14 17:44:47 -0400

    Create var_1.py

---
+++ var_1.py
@@ -0,0 +1,5 @@
+# Variables - 1. It stores values for later use
+#             2. It allows to give a label to a piece of data so that we can refer that data using the chosen variable name
+
+name = "Pooja"
+print(name)
