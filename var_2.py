cf0c2c119aef32b21a4107c928d3007ad4c21e93
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-14 17:59:32 -0400

    Create var_2.py

---
+++ var_2.py
@@ -0,0 +1,11 @@
+# Variables - 1. It stores values for later use
+#             2. It allows to give a label to a piece of data so that we can refer that data using the chosen variable name
+# Task : Check the length of the user input
+#    Using what you have learnt about the len() function and the input() function.
+#    Try to print out the number of characters in the user input.
+#    Write everything in just 1 line of code.
+# len() - returns the number of items in an object
+
+name = "Pooja"
+
+print(len(input("What is your name")))
