165591b476e9d288adbe8647933bdbf3f4560afa
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-14 18:10:59 -0400

    Create var_3.py

---
+++ var_3.py
@@ -0,0 +1,11 @@
+# Variables - 1. It stores values for later use
+#             2. It allows to give a label to a piece of data so that we can refer that data using the chosen variable name
+# len() - returns the number of items in an object
+# Task : Split each step in the previous exercise into a separate variable.
+# One variable called username and one called length.
+# Use the variable username in the len calculation.
+
+
+username = input("What is your name?")
+length = len(username)
+print(length)
