30cdd9fea71d14bd03d39ee4ea48b2208801c5c7
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-14 17:12:29 -0400

    Create debugging.py

---
+++ debugging.py
@@ -0,0 +1,13 @@
+# Fix the code below
+# print(Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters"
+# priint("String Concatenation is done with the + sign")
+# print(("New lines can be created with a \ and the letter n")
+
+# Debugging the above code withut any errors
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
