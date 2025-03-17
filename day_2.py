c1f808593ed43231c187ad61013da32d77010bf0
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-17 14:05:06 -0400

    Create day_2.py
    Different types of Data Types

---
+++ day_2.py
@@ -0,0 +1,18 @@
+# Subscripting - pulling out a particular element from a string
+print("Hello"[4])
+
+# String
+print("123" + "456") # concatenation
+
+# Integer = Whole numbers
+print(123 + 456)
+
+# Large Integers - ie., when we have large numbers (123,456,00) we separate the commas with underscores
+print(123_456_789)
+
+# Float - Floating Point Number
+print(3.14159)
+
+# Boolean - True / False
+print(True)
+print(False)
