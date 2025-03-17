6547a117100976ff1b6269dd94e3d25c8e7b2db7
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-17 15:45:12 -0400

    Create number_manipulation_2.py

---
+++ number_manipulation_2.py
@@ -0,0 +1,23 @@
+# Assignment Operators : To accumulates the results of our calculation
+# eg:
+score = 0
+
+# User scores a point
+score += 1
+print(score)
+
+score -= 1
+print(score)
+
+score *= 1
+print(score)
+
+score /= 1
+print(score)
+
+# f-strings : We can use f-strings to insert a variable or an expression into a string
+score = 0
+height = 2.4
+is_winning = True
+
+print(f"your score is = {score}, your height = {height}. you are winning is {True}")
