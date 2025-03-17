dbffda5fa88b1a1f0b7035ce2e48458806bc22ca
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-17 15:15:46 -0400

    Create math_operations.py

---
+++ math_operations.py
@@ -0,0 +1,22 @@
+ print("My age: " + str(12))
+
+ Mathematical Operations
+ Basic operators
+ print(2 ** 4) # exponents - 2 to the power 4 (calculates the power)
+ print(123 + 123)
+ print(5 - 3)
+ print(2 * 3)
+ print(6/3) # returns the value in float
+ print(6//3) # returns the value in int, but this is not recommended, except if the number has to return in int instead of flost
+
+# PEMDASLR : Parantheses, Exponents, Mul/Div, Add/Sub - LR (LeftRight)
+## ()
+## **
+## * OR /
+## + OR -
+
+# what is the output of this code
+print(3 * 3 + 3 / 3 - 3)
+
+# Change the above code so it outputs 3
+print(3 * (3 + 3) / 3 - 3)
