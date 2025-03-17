01d5fdc4d879f2cfd9885129ae29d8b5ab9aa1fd
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-17 14:25:07 -0400

    Create typeerrorandchecking.py

---
+++ typeerrorandchecking.py
@@ -0,0 +1,13 @@
+# Type error : These errors occur when you are using the wrong data type
+len(12345)
+
+# Correct
+len("Hello")
+
+# Type Checking : We can check the data type of any value or var in python using type() function
+print(type("Hello"))
+
+print(type("Hello"))
+print(type(123))
+print(type(12.34))
+print(type(True))
