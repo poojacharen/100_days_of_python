a7db4d292272a255d6724e424755a578fc41b08d
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-18 15:21:37 -0400

    Update nested_if_statements.py

--- nested_if_statements.py
+++ nested_if_statements.py
@@ -6,3 +6,6 @@ if <condition 1 is true>
         <do B>
         if <condition 3 is true>
             <do C>
+
+# In the nested if statements, A, B and C can all happen, but conditions 1, 2 and 3 must all be true. 
+# The computer will only check condition 2 if condition 1 is true.
