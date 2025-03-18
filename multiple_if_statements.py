b8df8d1d6a15d9a7c3f7b8aaa90411bd3c36ed88
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-18 15:19:58 -0400

    Create multiple_if_statements.py

---
+++ multiple_if_statements.py
@@ -0,0 +1,11 @@
+# Multiple If Statements
+
+if <condition 1 is true>
+    <do A>
+if <condition 2 is true>
+    <do B>
+if <condition 3 is true>
+    <do C>
+
+# In the multiple if statements, A, B, and C can all happen. But they are completely independent of each other. 
+# C can happen even if A and B don't. Every condition is checked, no matter the result of the others.
