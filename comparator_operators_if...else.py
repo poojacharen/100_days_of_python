db60b7b7e35ad57674b4472c25d1b2864e80093a
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-18 13:46:46 -0400

    Create comparator_operators_if...else.py

---
+++ comparator_operators_if...else.py
@@ -0,0 +1,12 @@
+# Comparator Operators
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+# == equal to
+# != not equal to
+
+if height >= 120:
+    print("You can ride")
+else:
+    print("You can't ride")
