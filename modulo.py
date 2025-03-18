e895bcdadb82a91ec18c1e5821dec78653f76002
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-18 14:13:34 -0400

    Create modulo.py

---
+++ modulo.py
@@ -0,0 +1,14 @@
+#The modulo operator gives you the remainder of a division.
+# 6 % 2 #will be 0
+# 6 % 5 #will be 1
+# 6 % 4 #will be 2
+
+
+print("Check Odd or Even!")
+modulo = int(input("Check whether it's odd or even"))
+
+if modulo % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
