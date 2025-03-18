ed61a667c256bdf32c1e4d18bd91b3c4695009de
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-18 15:20:57 -0400

    Update if_elif_else.py

--- if_elif_else.py
+++ if_elif_else.py
@@ -6,3 +6,6 @@ elif <condition 2 is true>
     <do B>
 else
     <do C>
+
+# In the if/elif/else block, only one of A, B, or C can happen, because if/elif/else are mutually exclusive. 
+# The first condition has to fail to go into the elif and the elif (or multiple elif) have to fail to go into the else. Condition 2 is only checked if condition 1 is false.
