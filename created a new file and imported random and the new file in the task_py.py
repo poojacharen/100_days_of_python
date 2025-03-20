11b8ced0a73b5c75c403660569e78ac0adc545e6
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-20 12:07:15 -0400

    Update created a new file and imported random and the new file in the task_py.py

--- created a new file and imported random and the new file in the task_py.py
+++ created a new file and imported random and the new file in the task_py.py
@@ -1,6 +1,6 @@
 # I have created a new python file (my_module) and written the code as
 # my_favourite_number = 1.2345
-# Now in the main file ie., task.py, I first we import random then the below code looks like
+# Now in the main file ie., task.py, I first we import random and also import the my_module (new file too) then the below code looks like
 
 import random
 import my_module
