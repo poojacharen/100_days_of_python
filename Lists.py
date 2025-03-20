4ce66c3929abe96e35be85861bb44edea74274ee
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-20 13:24:23 -0400

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,16 @@
+# Lists
+# You can create a simple collection of ordered items using a Python list. e.g.
+
+fruits = ["Apple", "Banana", "Orange", "Pear", "Cherry", "Guava", "Papaya", "Pomegranate"]
+
+ print(fruits)
+
+# I acn also print like ,
+ print(fruits[3])
+# It prints the fruit "Pear"
+
+# If I want to change the name of any fruit "Pomegranate" then I can write,
+ fruits[7] = "Promogranate"
+
+# If I want to add a new fruit to the list then,
+fruits.append("Watermelon")
