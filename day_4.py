0aa296ab5efc89e4eec33f19914e451edd31cc1f
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-20 11:53:29 -0400

    Create day_4.py

---
+++ day_4.py
@@ -0,0 +1,10 @@
+# Random Module
+# Think of the random module in Python as a tool that helps a computer make choices
+# that seem like guesses, kind of like rolling a dice or picking a card from a shuffled deck.
+
+import random
+
+# Random Whole Numbers within a range
+
+random_integer = random.randint(1,10)
+print(random_integer)
