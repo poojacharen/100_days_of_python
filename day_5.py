bac962ee2fe4507a648b2d619a8a940de34f539f
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-21 10:22:45 -0400

    Create day_5.py

---
+++ day_5.py
@@ -0,0 +1,9 @@
+# Loops allow us to tell the computer to repeat actions without having to write repeated code.
+# If we wanted the computer to print out 1 through to 100, it would be very painful to type a print 
+# statement for every number, or even just typing out all the numbers 1 through to 100. 
+# Loops allow us to create a rule and the computer can follow it to do a repeated action.
+
+# Syntax
+# for <variable name of each item> in <a List>:
+    # <do something>
+    # <do something else>
