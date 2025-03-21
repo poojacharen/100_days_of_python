289b4fd13ee91b7e6a96c20876d8b5fc8b1924a3
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-21 10:38:18 -0400

    Create Day 5 for_loop.py

---
+++ Day 5 for_loop.py
@@ -0,0 +1,8 @@
+# For Loops : It loops through a list and assigns a variable name to each of the items in the list
+#             in order.
+
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
