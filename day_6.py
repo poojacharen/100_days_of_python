94d603225ea5468b2709b2865004dab29e9147ce
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-24 15:09:53 -0400

    Create day_6.py

---
+++ day_6.py
@@ -0,0 +1,8 @@
+# Functions : A function in its simplest form is just a wrapper name for a block of code.
+# You give it name and then when you call the function by that name, all the code within the
+# function block will be executed. It can help us save time and reduce repeated code.
+# Eg:
+
+print("Hello")
+name_len = len("pooja")
+print(name_len)
